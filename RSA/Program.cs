b7b0162f58585ee854d5279d423cc2050212d7df
Anilam <troyanka-girl@yandex.ru>
2016-01-20 13:45:29 +0100

    WTF

--- RSA/Program.cs
+++ RSA/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IntXLib;
+using System.Timers;
+using System.Diagnostics;
 
 
 namespace RSA
@@ -13,9 +15,19 @@ namespace RSA
         static void Main(string[] args)
         {
             RSA rsa = new RSA("nothinghere");
-           // rsa.GeneratePQ();
+            // rsa.GeneratePQ();
             long temp = 32452834762618763;
-            System.Console.WriteLine(temp + ((MathAlgs.PrimarilyTestBruteForce(new IntX(temp)))==true ? "is prime": "isnot prime"));
+            Stopwatch stw = new Stopwatch();
+            stw.Start();
+            System.Console.WriteLine(temp + ((MathAlgs.PrimarilyTestBruteForceIncreased(new IntX(temp))) == true ? "is prime" : "isnot prime"));
+            stw.Stop();
+            System.Console.WriteLine(stw.ElapsedTicks);
+
+            stw.Restart();
+            System.Console.WriteLine(temp + ((MathAlgs.PrimarilyTestBruteForce(new IntX(temp))) == true ? "is prime" : "isnot prime"));
+            stw.Stop();
+            System.Console.WriteLine(stw.ElapsedTicks);
+
             System.Console.Read();
         }
     }
@@ -35,10 +47,12 @@ namespace RSA
         private string PMessage;
         public string CMessage;
 
-        public RSA(string message):this(message,2048)
-        {  }
+        public RSA(string message)
+            : this(message, 2048)
+        { }
 
-        public RSA(string message, int bitsize):this(message,2048,0)
+        public RSA(string message, int bitsize)
+            : this(message, 2048, 0)
         { }
 
         public RSA(string message, int bitsize, int securityLevel)
@@ -57,7 +71,7 @@ namespace RSA
         /*Secured?*/
         public IntX GeneratePrime(IntX maximum)
         {
-           return this.GeneratePrime(maximum.ToString(2).Length-1);
+            return this.GeneratePrime(maximum.ToString(2).Length - 1);
         }
 
         public IntX GeneratePrime(int bitsize = 0)
@@ -86,16 +100,16 @@ namespace RSA
         /**/
         public IntX GenerateCoprime(IntX mod)
         {
-          /*  Random rnd = new Random(Guid.NewGuid().GetHashCode());
-            int sizeCurr = sizeof(Int32);
-            IntX temp = new IntX(rnd.Next(Int32.MaxValue));
-            while (sizeCurr <= _bitsize / 8)
-            {
-                temp = (temp << 32) + rnd.Next(Int32.MaxValue);
-                sizeCurr += sizeof(Int32);
-                System.Console.WriteLine(sizeCurr);
-            }
-            */
+            /*  Random rnd = new Random(Guid.NewGuid().GetHashCode());
+              int sizeCurr = sizeof(Int32);
+              IntX temp = new IntX(rnd.Next(Int32.MaxValue));
+              while (sizeCurr <= _bitsize / 8)
+              {
+                  temp = (temp << 32) + rnd.Next(Int32.MaxValue);
+                  sizeCurr += sizeof(Int32);
+                  System.Console.WriteLine(sizeCurr);
+              }
+              */
             return null;
         }
 
@@ -104,7 +118,7 @@ namespace RSA
             IntX temp = new IntX();
             while (temp != 1)
             {
-                
+
             }
         }
 
@@ -121,7 +135,7 @@ namespace RSA
         {
             throw new NotImplementedException();
         }
-        
+
         private void GenerateN()
         {
             this.n = this.p * this.q;
@@ -171,6 +185,44 @@ namespace RSA
             return true;
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lst"></param>
+        /// <param name="elem"></param>
+        /// <returns>false if the one of the elements is divisible by the elements existing in array; else true</returns>
+        public static bool ListSearch(List<IntX> lst, IntX elem)
+        {
+            foreach (var _elemLst in lst)
+            {
+                if (elem % _elemLst == 0)
+                    return false;
+            }
+            return true;
+        }
+        public static bool PrimarilyTestBruteForceIncreased(IntX number)
+        {
+            if (number <= 2)
+                return false;
+            List<IntX> _tempList = new List<IntX>();
+            IntX temp = new IntX(3);
+            if (number % 2 == 0)
+                return false;
+            while (temp * temp <= number)
+            {
+                if (number % temp == 0)
+                    return false;
+                else
+                {
+                    _tempList.Add(temp);
+                    do
+                    {
+                        temp += 2;
+                    }
+                    while (!ListSearch(_tempList, temp));
+                }
+            }
+            return true;
+        }
     }
 }
