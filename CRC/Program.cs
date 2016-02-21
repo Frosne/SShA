7c754fd5fb104d8dac12871687cd5ecd6d96a638
Anilam <troyanka-girl@yandex.ru>
2016-02-21 12:57:48 +0100

    Access changed

--- CRC/Program.cs
+++ CRC/Program.cs
@@ -9,6 +9,10 @@ namespace CRC
 {
     class Program
     {
+        /// <summary>
+        /// Example of use
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             string text = "1101011011";
@@ -22,32 +26,51 @@ namespace CRC
 
     class CRC
     {
-        public BitArray ExtendedText;
+        // Text extended with 0's
+        private BitArray ExtendedText;
         public BitArray Key;
-        public BitArray KeyExtended;
-
+        //shifted  key
+        private BitArray KeyExtended;
+        /// <summary>
+        /// Basic constructor
+        /// </summary>
+        /// <param name="Text">Text to get hash</param>
+        /// <param name="Key">Polynomial key</param>
         public CRC(string Text, string Key)
         {
            ExtendToSizeLeft(ref this.ExtendedText, ToBitArray(Text), Text.Length + Key.Length - 1);
            this.Key = ToBitArray(Key);
            this.KeyExtended = new BitArray(Text.Length + Key.Length - 1);
         }
-
+        /// <summary>
+        /// Empty contstructot
+        /// </summary>
         public CRC()
         {
            
         }
-
-        
-        public void ExtendToSizeRight(ref BitArray to, BitArray bitArray, int p)
+        /// <summary>
+        /// aaa -> 000aaa
+        /// </summary>
+        /// <param name="to">Reference to array to put the result</param>
+        /// <param name="bitArray">Array from where to put</param>
+        /// <param name="p">Size of the new array</param>
+        private void ExtendToSizeRight(ref BitArray to, BitArray bitArray, int p)
         {
             if (to == null)
                 to = new BitArray(p, false);
             for (int i = 0; i < bitArray.Length; i++)
                 to[i] = bitArray[i];
         }
-
-        public BitArray ExtendToSizeLeft(ref BitArray to, BitArray bitArray, int p, int counter = 0)
+        /// <summary>
+        /// aaa->aaa000
+        /// </summary>
+        /// <param name="to"></param>
+        /// <param name="bitArray"></param>
+        /// <param name="p"></param>
+        /// <param name="counter"></param>
+        /// <returns></returns>
+        private BitArray ExtendToSizeLeft(ref BitArray to, BitArray bitArray, int p, int counter = 0)
         {
            // if (to == null)
                 to = new BitArray(p, false);
@@ -57,7 +80,11 @@ namespace CRC
             }
             return to;
         }
-
+        /// <summary>
+        /// Convert String to BitArray
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <returns></returns>
         public static BitArray ToBitArray(string Text)
         {
             BitArray br = new BitArray(Text.Length);
@@ -74,22 +101,32 @@ namespace CRC
             }
             return br;
         }
-
         public static void PrintBitArray(BitArray br)
         {
             for (int i = br.Length - 1; i >= 0; i--)
                 System.Console.Write(br[i] ==  true ?  "1" : "0" );
             System.Console.WriteLine();
         }
-
-        public void GenerateOneRound(ref BitArray to, BitArray key, int counter)
+        /// <summary>
+        /// Generate one xor round. 
+        /// </summary>
+        /// <param name="to">Where to put the result</param>
+        /// <param name="key">Key used</param>
+        /// <param name="counter">Shifter of the key</param>
+        private void GenerateOneRound(ref BitArray to, BitArray key, int counter)
         {
             this.ExtendToSizeLeft(ref this.KeyExtended, key, to.Length, counter);
             to = to.Xor(this.KeyExtended);
 
 
         }
-
+        /// <summary>
+        /// Basic algorithm
+        /// </summary>
+        /// <param name="to">Where to put the result</param>
+        /// <param name="key">Key used</param>
+        /// <param name="Truncated">Result</param>
+        /// <returns></returns>
         public BitArray CRCAlg(ref BitArray to, BitArray key , out string Truncated)
         {
             this.Key = key;
@@ -129,6 +166,7 @@ namespace CRC
             }
             return null;
         }
+       
         public BitArray CRCAlg(string Text, string Key, out string Truncated)
         {
             Truncated = String.Empty;
